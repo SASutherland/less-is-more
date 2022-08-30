d98c9206803b6fd7918d7b884c1c020628ad8330
Carole Gendron <carole.gendron@gmail.com>
2022-08-30 14:32:00 +0200

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get '/survey', to: "survey#index"
+  get '/survey', to: "survey#index", as: :survey
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :ideas do
     resources :favourites
