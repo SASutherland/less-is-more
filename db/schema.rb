c227681208dc3fdc29e9feaee680c5e94ad68868
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-30 12:33:23 +0200

    Created models, added sample seed file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_134701) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_095346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "idea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_favourites_on_idea_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "location"
+    t.string "photo"
+    t.integer "votes"
+    t.string "category"
+    t.string "subcategory"
+    t.string "impact"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +46,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_134701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "photo"
+    t.string "country"
+    t.string "transport"
+    t.string "food"
+    t.string "housing"
+    t.string "leisure"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "ideas"
+  add_foreign_key "favourites", "users"
+  add_foreign_key "ideas", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,184 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Clearing database..."
+Favourite.destroy_all
+Idea.destroy_all
+User.destroy_all
+puts "Cleared!"
+
+puts "Creating users, ideas..."
+
+ideaname1 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Brian",
+    last_name: "Mac",
+    email: "brian@fakemail.com",
+    password: "password",
+  )
+)
+file = URI.open("")
+ideaname1.photo.attach(io: file, filename: "ideaname1.jpg", content_type: "image/jpg")
+ideaname1.save!
+
+ideaname2 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Janet",
+    last_name: "Marsh",
+    email: "janet@fakemail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname2.photo.attach(io: file, filename: "ideaname2.jpg", content_type: "image/jpg")
+ideaname2.save!
+
+ideaname3 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Dan",
+    last_name: "Goodman",
+    email: "dan@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname3.photo.attach(io: file, filename: "ideaname3.jpg", content_type: "image/jpg")
+ideaname3.save!
+
+ideaname4 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Michael",
+    last_name: "Loev",
+    email: "mike@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname4.photo.attach(io: file, filename: "ideaname4.jpg", content_type: "image/jpg")
+ideaname4.save!
+
+ideaname5 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Donna",
+    last_name: "Hunter",
+    email: "donna@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname5.photo.attach(io: file, filename: "ideaname5.jpg", content_type: "image/jpg")
+ideaname5.save!
+
+ideaname6 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Sally",
+    last_name: "Mantis",
+    email: "sally@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname6.photo.attach(io: file, filename: "ideaname6.jpg", content_type: "image/jpg")
+ideaname6.save!
+
+ideaname7 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Todd",
+    last_name: "Gakke",
+    email: "todd@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname7.photo.attach(io: file, filename: "ideaname7.jpg", content_type: "image/jpg")
+ideaname7.save!
+
+ideaname8 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Lois",
+    last_name: "Steele",
+    email: "lois@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname8.photo.attach(io: file, filename: "ideaname8.jpg", content_type: "image/jpg")
+ideaname8.save!
+
+ideaname9 = Idea.new(
+  title: ,
+  content: ,
+  location: ,
+  votes: ,
+  category: ,
+  subcategory: ,
+  impact: ,
+  user: User.create!(
+    first_name: "Jake",
+    last_name: "Payne",
+    email: "jake@realmail.com",
+    password: "password"
+  )
+)
+file = URI.open("")
+ideaname9.photo.attach(io: file, filename: "ideaname9.jpg", content_type: "image/jpg")
+ideaname9.save!
+
+puts "Done!"
