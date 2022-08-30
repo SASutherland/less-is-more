1f2fd6c0233c6671239883b9202c7048ba8f87cc
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-30 15:04:37 +0200

    Added Survey method.

--- app/controllers/surveys_controller.rb
+++
@@ -1,6 +0,0 @@
-class SurveysController < ApplicationController
-
-  def index
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get '/survey', to: "survey#index"
+  get '/survey', to: "pages#survey"
+  get '/results', to: "pages#results"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :ideas do
-    resources :favourites
+  resources :ideas, except: :destroy do
+    collection do
+      get 'my-ideas'
+    end
+    resources :favourites, only: :create
   end
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :favourites, only: [:index, :destroy]
 end
