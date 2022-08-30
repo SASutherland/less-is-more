016e523c1db92928f3199cade45df6c43b517b6d
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-30 13:46:41 +0200

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/survey', to: "survey#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :ideas do
-    resources :favourites, only: [:show, :create]
+    resources :favourites
   end
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :favourites, only: [:edit, :update, :destroy]
 end
