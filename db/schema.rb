91299a250c9bf1acc048999745d2b9a12183239d
Shawn Sutherland <shawnsutherland@hotmail.com>
2022-08-31 15:56:25 +0200

    Almost got a globe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_095346) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_092431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_095346) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
