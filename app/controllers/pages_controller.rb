bbed71bace44228f78e89999cef15b537dd16df0
Carole Gendron <carole.gendron@gmail.com>
2022-08-31 18:03:10 +0200

    survey with answer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,21 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+
   end
 
   def survey
+
   end
+
+  def results
+
+    @country = params[:country]
+    @work = params[:work]
+    @flight = params[:flight]
+    @food = params[:food]
+    @myRange = params[:myRange]
+    @leisure = params[:leisure]
+  end
+
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,26 +1,32 @@
 // Entry point for the build script in your package.json
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import Typed from 'typed.js'
-
-var options = {
-  strings:  ['impact calculator', 'performance tracker','idea generator','community builder'],
-  typeSpeed: 60,
-  loop: true
-}
-var typed = new Typed("#typed", options);
 
 
-//Slider form//
+
+
+
+
+// var options = {
+//   strings:  ['impact calculator', 'performance tracker','idea generator','community builder'],
+//   typeSpeed: 60,
+//   loop: true
+// }
+// var typed = new Typed("#typed", options);
+
+
+// import { initSortable } from "../javascript/plugin/init_sortable"
+// initSortable()
+
+//
+
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  console.log()
-  output.innerHTML = this.value;
-}
+slider.addEventListener("input", (event) => {output.innerText = event.target.value;})
+output.innerText = slider.value; // Display the default slider value
+console.log(slider.value)
 
-$("#sortable").sortable();
