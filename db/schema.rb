dcefbe0f3e6b5a817c272c6ad3ae6cfd892f01f7
Cristina Nunu <nunucristina0@gmail.com>
2022-09-07 14:03:07 +0200

    Payments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_07_084040) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_084040) do
     t.integer "votes"
     t.string "category"
     t.string "subcategory"
-    t.float "impact"
+    t.integer "impact"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
